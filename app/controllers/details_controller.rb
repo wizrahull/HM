3f14e6bddb5eef3b575102af2555eb104c700fb7
wizrahull <purirahul31@gmail.com>
2022-07-12 15:17:01 +0530

    main fix heroku

--- app/controllers/details_controller.rb
+++ app/controllers/details_controller.rb
@@ -1,75 +1,75 @@
-# class DetailsController < ApplicationController
-#   before_action :set_detail, only: %i[ show edit update destroy ]
+class DetailsController < ApplicationController
+  before_action :set_detail, only: %i[ show edit update destroy ]
 
-#   # GET /details or /details.json
-#   def index
-#     @u=Login.find(1)
-#     @detail=@u.Detail.all
-#   end
+  # GET /details or /details.json
+  def index
+    @u=Login.find(1)
+    @detail=@u.Detail.all
+  end
 
-#   # GET /details/1 or /details/1.json
-#   def show
-#     @login=Login.all
-#     @login.Detail.find(params[:id])
-#   end
+  # GET /details/1 or /details/1.json
+  def show
+    @login=Login.all
+    @login.Detail.find(params[:id])
+  end
 
-#   # GET /details/new
-#   def new
-#     @detail = Detail.new
-#   end
+  # GET /details/new
+  def new
+    @detail = Detail.new
+  end
 
-#   # GET /details/1/edit
-#   def edit
-#   end
+  # GET /details/1/edit
+  def edit
+  end
 
-#   # POST /details or /details.json
-#   def create
-#     # @detail = Detail.new(detail_params)
-#     @detail=Detail.create(number: params[:number],city: params[:city],state: params, login_id: @login.id)
+  # POST /details or /details.json
+  def create
+    # @detail = Detail.new(detail_params)
+    @detail=Detail.create(number: params[:number],city: params[:city],state: params, login_id: @login.id)
     
-#     @detail.save
-#     # respond_to do |format|
-#     #   if @detail.save
-#     #     format.html { redirect_to detail_url(@detail), notice: "Detail was successfully created." }
-#     #     format.json { render :show, status: :created, location: @detail }
-#     #   else
-#     #     format.html { render :new, status: :unprocessable_entity }
-#     #     format.json { render json: @detail.errors, status: :unprocessable_entity }
-#     #   end
-#     # end
-#   end
+    @detail.save
+    # respond_to do |format|
+    #   if @detail.save
+    #     format.html { redirect_to detail_url(@detail), notice: "Detail was successfully created." }
+    #     format.json { render :show, status: :created, location: @detail }
+    #   else
+    #     format.html { render :new, status: :unprocessable_entity }
+    #     format.json { render json: @detail.errors, status: :unprocessable_entity }
+    #   end
+    # end
+  end
 
-#   # PATCH/PUT /details/1 or /details/1.json
-#   def update
-#     respond_to do |format|
-#       if @detail.update(detail_params)
-#         format.html { redirect_to detail_url(@detail), notice: "Detail was successfully updated." }
-#         format.json { render :show, status: :ok, location: @detail }
-#       else
-#         format.html { render :edit, status: :unprocessable_entity }
-#         format.json { render json: @detail.errors, status: :unprocessable_entity }
-#       end
-#     end
-#   end
+  # PATCH/PUT /details/1 or /details/1.json
+  def update
+    respond_to do |format|
+      if @detail.update(detail_params)
+        format.html { redirect_to detail_url(@detail), notice: "Detail was successfully updated." }
+        format.json { render :show, status: :ok, location: @detail }
+      else
+        format.html { render :edit, status: :unprocessable_entity }
+        format.json { render json: @detail.errors, status: :unprocessable_entity }
+      end
+    end
+  end
 
-#   # DELETE /details/1 or /details/1.json
-#   def destroy
-#     @detail.destroy
+  # DELETE /details/1 or /details/1.json
+  def destroy
+    @detail.destroy
 
-#     respond_to do |format|
-#       format.html { redirect_to details_url, notice: "Detail was successfully destroyed." }
-#       format.json { head :no_content }
-#     end
-#   end
+    respond_to do |format|
+      format.html { redirect_to details_url, notice: "Detail was successfully destroyed." }
+      format.json { head :no_content }
+    end
+  end
 
-#   private
-#     # Use callbacks to share common setup or constraints between actions.
-#     def set_detail
-#       @detail = Detail.find(params[:id])
-#     end
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_detail
+      @detail = Detail.find(params[:id])
+    end
 
-#     # Only allow a list of trusted parameters through.
-#     def detail_params
-#       params.require(:detail).permit(:number, :city, :state)
-#     end
-# end
+    # Only allow a list of trusted parameters through.
+    def detail_params
+      params.require(:detail).permit(:number, :city, :state)
+    end
+end
